861a617d1bfad0abd7369b43d008e21070da6fb4
이민진 <e.minjin00@gmail.com>
2022-01-10 06:11:07 +0900

    Update License for educational use

--- calculator.py
+++ calculator.py
@@ -9,9 +9,17 @@ def multiply(a, b):
 	return a * b
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 def divide_premium(a, b):
 =======
 def divide_free(a, b):
 >>>>>>> 6be1c54... Change divide function as free
     return a / b
     
+=======
+def divide_new(a, b):
+    return a / b
+
+def say_hello():
+    print("hello")
+>>>>>>> 4485d2d... Add say_hello function
