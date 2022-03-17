ff63f1847a4c33e921ae7059957efd58a75fc428
이민진 <mj@codeit.kr>
2022-03-17 21:39:13 +0900

    conflict 해결

--- calculator.py
+++ calculator.py
@@ -8,42 +8,17 @@ def substract(a, b):
 def multiply(a, b):
 	return a * b
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-def divide_premium(a, b):
-=======
-def divide_free(a, b):
->>>>>>> 6be1c54... Change divide function as free
-    return a / b
-    
-=======
 def divide_new(a, b):
     return a / b
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-def say_hello():
-    print("hello")
->>>>>>> 4485d2d... Add say_hello function
-=======
->>>>>>> ad5957a... Remove say_hello function in calculator.py
-=======
 
 def get_Median(a, b):
     return (a + b) / 2
-<<<<<<< HEAD
->>>>>>> c2ba8e6... Add get_Median function
-=======
 
 def get_Remainder(a, b):
     return a // b
-<<<<<<< HEAD
->>>>>>> c446cb5... Add get_Remainder function
-=======
 
 def get_Abs(a, b):
     if num == 0:
         return num
     else:
         return -num
->>>>>>> 343e434... Add get_Abs functiono
